c669af9eb7c9e4577213400e4361d26b50bce29e
MaximeG709lln <MaximeG709@LLN.CampusSintUrsula.be>
2026-05-13 10:06:39 +0200

    Persoon Opdracht start

--- TJM/Program.cs
+++ TJM/Program.cs
@@ -1,2 +1,26 @@
 ﻿Console.WriteLine("Hello, World!");
 Console.WriteLine("Funky nugget is cool.");
+
+class Persoon
+{
+    public string naam = "Jasper";
+    public int leeftijd = 18;
+    public string geslacht ="M";
+    public string nation = "NLD";
+    public string job = "Student";
+
+    public void Groet()
+    {
+        // Veranderd op basis van taal/nationaliteit
+        
+        if (nation = "NLD")
+        {
+            Console.WriteLine($"Gegroet, ik ben {naam}.");
+        } else
+        {
+            Console.WriteLine($"Salutations, I'm {naam}.");
+        }
+    }
+
+    // Andere methods hier
+}
